775da6cd8e76b14863b2842a80095a2b655f2e62
Alex Vanderpot <alex@vanderpot.com>
2022-01-13 18:09:30 -0800

    tweak

--- generate.sh
+++ generate.sh
@@ -2,9 +2,9 @@
 
 if ! command -v kompose &> /dev/null
 then
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.1/kompose-linux-amd64 -o kompose
-chmod +x kompose
-sudo mv ./kompose /usr/local/bin/kompose
+    curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.1/kompose-linux-amd64 -o kompose
+    chmod +x kompose
+    sudo mv ./kompose /usr/local/bin/kompose
 fi
 
 pushd seedbox

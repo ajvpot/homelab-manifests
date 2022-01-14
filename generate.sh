3f000b007fad5e03cec58c5ec0d31aa37220c4b9
Alex Vanderpot <alex@vanderpot.com>
2022-01-13 18:07:29 -0800

    install kompose

--- generate.sh
+++ generate.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+if ! command -v kompose &> /dev/null
+then
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.1/kompose-linux-amd64 -o kompose
+chmod +x kompose
+sudo mv ./kompose /usr/local/bin/kompose
+fi
+
 pushd seedbox
 kompose convert -c --volumes hostPath
 popd
